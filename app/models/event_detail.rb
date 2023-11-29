2756f670b008a40796f3fae8b2ae99bb7b6aa91f
apurvamandalika-tamu <apurva.mandalika@tamu.edu>
2023-11-29 16:51:39 -0600

    made event detail form to take blanks and changed school name and CF distributed

--- app/models/event_detail.rb
+++ app/models/event_detail.rb
@@ -1,23 +1,23 @@
 # frozen_string_literal: true
 
 class EventDetail < ApplicationRecord
-  validates :EventDate, presence: true
-  validates :School, presence: true
-  validates :ConsentFD, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :DenHrs, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :DenTravelHrs, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :DenTravelMil, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :HygHours, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :HygTravelMil, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :HygTravelHrs, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :AssistantTravelMil, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :AssistantHrs, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :AssistantTravelHrs, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :OtherHrs, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :OtherTravelHrs, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :OtherTravelMiles, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :NumberOfSSPDriven, presence: true, numericality: { greater_than_or_equal_to: 0 }
-  validates :TotalMilesDriven, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :EventDate, presence: false, allow_blank: true
+  validates :School, presence: false, allow_blank: true
+  validates :ConsentFD, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :DenHrs, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :DenTravelHrs, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :DenTravelMil, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :HygHours, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :HygTravelMil, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :HygTravelHrs, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :AssistantTravelMil, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :AssistantHrs, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :AssistantTravelHrs, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :OtherHrs, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :OtherTravelHrs, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :OtherTravelMiles, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :NumberOfSSPDriven, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :TotalMilesDriven, presence: false, allow_blank: true, numericality: { greater_than_or_equal_to: 0 }
   # Not needed in event details page
   validates :ChildScreened, presence: false, allow_blank: true
   validates :ChildReceivingSealant, presence: false, allow_blank: true
